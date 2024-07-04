30aaa71d1847f0fdcbdca78b75087b45f24c8e97
Hansika Karn <133098410+Hansikarn@users.noreply.github.com>
2024-07-04 12:02:22 +0000

    Added a form

--- src/components/Meals/MealItem/MealItem.jsx
+++ src/components/Meals/MealItem/MealItem.jsx
@@ -1,4 +1,5 @@
 import classes from "./MealItem.module.css";
+import MealItemForm from "./MealItemForm";
 
 const MealItem = (props) => {
   const Price = `$${props.price.toFixed(2)}`;
@@ -9,7 +10,9 @@ const MealItem = (props) => {
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{Price}</div>
       </div>
-      <div></div>
+      <div>
+        <MealItemForm />
+      </div>
     </li>
   );
 };
